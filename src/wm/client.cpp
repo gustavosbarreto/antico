ea88b45cd0ca670c252f7f659da931bf95af8a33
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2010-07-16 11:57:14 -0300

    Revert "Revert "Enables styleable decoration""
    This reverts commit 634153c3154be82543f74a0e805a5e8882b8cf21.

--- src/wm/client.cpp
+++ src/wm/client.cpp
@@ -2,6 +2,7 @@
 #include "windowmanager.hpp"
 #include "decorations/ubuntu/ubuntudecoration.hpp"
 #include "decorations/panther/pantherdecoration.hpp"
+#include "decorations/styleable/styleabledecoration.hpp"
 #include "atoms.hpp"
 #include "fixx11h.h"
 
@@ -31,7 +32,7 @@ Client::Client(Qt::HANDLE winId, QObject *parent)
       Once compiled it will not be possible to change the decoration unless the decoration be a engine (like KWin)
     */
 
-    _decoration = new UbuntuDecoration(this);
+    _decoration = new StyleableDecoration(this);
 
     // Allow composited clients
     if (WindowManager::self()->compositeManagerIsRunning())
