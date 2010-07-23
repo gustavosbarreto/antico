11765c5f9a173d098840428b45bc283acafee659
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2010-07-22 21:22:33 -0300

    Set pixmap button with CSS

--- src/wm/decorations/styleable/titlebarbutton.cpp
+++ src/wm/decorations/styleable/titlebarbutton.cpp
@@ -26,27 +26,6 @@ TitleBarButton::TitleBarButton(StyleableDecoration::ButtonType type, QWidget *pa
 
 void TitleBarButton::setActive(bool active)
 {
-    switch (_type)
-    {
-        case StyleableDecoration::CloseButton:
-	    if (active)
-		setPixmap(QPixmap(":/decorations/styleable/images/close.png"));
-	    else
-		setPixmap(QPixmap(":/decorations/styleable/images/close_unfocused.png"));
-            break;
-        case StyleableDecoration::MinimizeButton:
-	    if (active)
-		setPixmap(QPixmap(":/decorations/styleable/images/minimize.png"));
-	    else
-		setPixmap(QPixmap(":/decorations/styleable/images/minimize_unfocused.png"));
-            break;
-        case StyleableDecoration::MaximizeButton:
-	    if (active)
-		setPixmap(QPixmap(":/decorations/styleable/images/maximize.png"));
-	    else
-		setPixmap(QPixmap(":/decorations/styleable/images/maximize_unfocused.png"));
-            break;
-    }
 }
 
 void TitleBarButton::mousePressEvent(QMouseEvent *event)
